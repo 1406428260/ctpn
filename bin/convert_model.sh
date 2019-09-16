8984466c4e98af8ab11e3aae9f133b3f9500364d
piginzoo <piginzoo@gmail.com>
2019-09-16 14:47:52 +0800

    update

--- bin/convert_model.sh
+++ bin/convert_model.sh
@@ -8,7 +8,7 @@ save_mod_dir=$2
 if [ "$ckpt_mod_path" = "" ]; then
     echo "ckpt模型目录不能为空: conver_model <原始模型的名称> <目标pb模型的生成目录>"
     echo "例："
-    echo "  convert_model.sh /app.fast/projects/models/ctpn-2019-05-28-22-32-39-2901.ckpt ./model"
+    echo "  bin/convert_model.sh /app.fast/projects/models/ctpn-2019-05-28-22-32-39-2901.ckpt ../ocr/model/ctpn"
     exit
 fi
 
